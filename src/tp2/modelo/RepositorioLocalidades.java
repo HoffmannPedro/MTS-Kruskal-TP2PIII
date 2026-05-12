f89739fb1e0003093c58bca95ba0c5b0a5f41409
ValenArri <valemare2005@gmail.com>
2026-05-12 19:07:07 -0300

    Revert "Merge branch 'main' of https://github.com/HoffmannPedro/MTS-Kruskal-TP2PIII"
    This reverts commit 45f07fc7f4dacf170b13f110adce0617f961dad1, reversing
    changes made to cb49f61238acbed3a5d7bd0ebb2410c90c4804ba.

--- src/tp2/modelo/RepositorioLocalidades.java
+++
@@ -1,9 +0,0 @@
-package tp2.modelo;
-
-import java.util.List;
-
-/** Interfaz para la persistencia de localidades */
-public interface RepositorioLocalidades {
-    void guardar(List<Localidad> localidades);
-    List<Localidad> cargar();
-}
